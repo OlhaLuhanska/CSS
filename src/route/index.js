43146c202e5ea0c040627221a3a8f47c9159f777
OlhaLuhanska <olyaelk@gmail.com>
2023-05-30 18:25:16 +0300

    selectors4

--- src/route/index.js
+++ src/route/index.js
@@ -3874,6 +3874,16 @@ router.get('/css2', function (req, res) {
   })
   //                  ↑↑ сюди вводимо JSON дані
 })
+
+router.get('/css3', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('css3', {
+    layout: 'bootstrap',
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
 // ================================================================
 
 // Підключаємо роутер до бек-енду
